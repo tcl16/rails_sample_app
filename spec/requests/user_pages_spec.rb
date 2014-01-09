abaa25d05845268335a40bed6362eed41dee95d3
Thomas Lent <tcl293@gmail.com>
2014-01-09 17:21:17 -0500

    cleanup tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -31,13 +31,7 @@ describe "User pages" do
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name", with: "Example User"
-        fill_in "Email", with: "user@example.com"
-        fill_in "Password", with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
-
+      before { valid_signup_fill }
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
@@ -48,7 +42,7 @@ describe "User pages" do
 
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_success_message('Welcome')}
       end
     end
   end
